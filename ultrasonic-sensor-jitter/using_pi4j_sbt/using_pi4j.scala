f27eb731042bb84cb573b33c5ea1b0d15be68d62
mrkneller <mrkneller@hotmail.com>
2016-12-25 14:32:10 +0000

    Split ammonite-scala script up so that I can reuse the impl from a full scala project

---
+++ ultrasonic-sensor-jitter/using_pi4j_sbt/using_pi4j.scala
@@ -0,0 +1,40 @@
+import com.pi4j.io.gpio._
+import java.time.Instant
+import java.time.temporal.ChronoUnit.NANOS
+
+object UsingPi4j {
+
+  def main(): Unit = {
+
+				GpioFactory.setDefaultProvider(new RaspiGpioProvider(RaspiPinNumberingScheme.BROADCOM_PIN_NUMBERING))
+
+				val controller = GpioFactory.getInstance()
+				val trigger = controller.provisionDigitalOutputPin(RaspiBcmPin.GPIO_17)
+				val echo = controller.provisionDigitalInputPin(RaspiBcmPin.GPIO_18)
+
+				def distance(): Double = {
+
+					// clear trigger
+					trigger.low()
+					Thread.sleep(0, 2000)
+					
+					// perform a 10 microsecond pulse
+					trigger.high()
+					Thread.sleep(0, 10000)
+					trigger.low()
+
+					while (echo.isLow()) {}
+					val rise_time = Instant.now()
+					while (echo.isHigh()) {}
+					val fall_time = Instant.now()
+
+					NANOS.between(rise_time, fall_time).toDouble / 1000000000 * 340 / 2
+				}
+
+				val N = 1000
+
+				(0 until N) foreach { i =>
+					println(s"$i: ${distance()} m")
+				}
+  }
+}
