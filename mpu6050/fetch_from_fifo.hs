93f9329d45211663155d7a7c6b74f003b5ecc55e
petekneller <mrkneller@hotmail.com>
2017-01-06 23:23:08 +0000

    Fiddle around with sampling rate to see how that affects reading the FIFO byte-at-a-time

--- mpu6050/fetch_from_fifo.hs
+++ mpu6050/fetch_from_fifo.hs
@@ -2,7 +2,7 @@ module Main where
 
 import System.IO.SMBus
 import Data.Bits
-import System.CPUTime
+import Data.Time.Clock
 import Text.Printf
 
 dev = Device 0x68
@@ -16,13 +16,16 @@ fetchFifo bus dev = read_byte_data bus dev '\x74'
 
 doIt bus dev = do
   cnt <- fetchFifoCount bus dev
+  t1 <- getCurrentTime
   _ <- mapM_ (\_ -> fetchFifo bus dev) $ take cnt (repeat 1)
-  putStrLn $ printf "Fetched %d bytes from FIFO" cnt
+  t2 <- getCurrentTime
+  let delta = (realToFrac ((utctDayTime t2) - (utctDayTime t1))) :: Double
+  putStrLn $ printf "Fetched %4d bytes from FIFO and took %f seconds" cnt delta
 
 main = withSMBus 1 $ \bus -> do
   _ <- write_byte_data bus dev '\x6B' '\x00' -- wake up device
   _ <- write_byte_data bus dev '\x23' '\x40' -- enable gyro X to FIFO
   _ <- write_byte_data bus dev '\x6A' '\x40' -- enable FIFO
   _ <- write_byte_data bus dev '\x1A' '\x00' -- set DLPF off
-  _ <- write_byte_data bus dev '\x19' '\x07' -- set sampling divider = 8
+  _ <- write_byte_data bus dev '\x19' ((toEnum 7) :: Char) -- set sampling divider = x + 1
   mapM_ (\_ -> doIt bus dev) $ repeat 1
